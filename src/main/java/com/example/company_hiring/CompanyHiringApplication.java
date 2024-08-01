c526a7897051164f4f6a17d9c8fbe426200e50e1
hyojin <dongurami.kim@gmail.com>
2024-08-01 18:39:45 +0900

    init: init project

---
+++ src/main/java/com/example/company_hiring/CompanyHiringApplication.java
@@ -0,0 +1,13 @@
+package com.example.company_hiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyHiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyHiringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/company_hiring/CompanyHiringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.company_hiring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyHiringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
